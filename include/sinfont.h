d1eda3ac3f39475c82195628dd1afc3aaca2e78d
svn <svn@d9b11391-c972-1f02-32a5-2912da4355a6>
2009-07-14 04:59:21 +0000

    yarr

--- include/sinfont.h
+++ include/sinfont.h
@@ -3,10 +3,12 @@
 
 #include <ft2build.h>
 #include FT_FREETYPE_H
-#include <vector>
 #include <GL/glfw.h>
 
 #include <list>
+#include <map>
+#include <vector>
+
 
 class glyph_info {
 public:
@@ -41,7 +43,6 @@ typedef struct glyph_matrixs {
 class freetype_font {
 public:
 
-
     int init(char*,int,bool);
     int createText( char *, std::vector<GLfloat>*, float, int, int, int );
     glyph_matrix* returnCharMatrix( char );
@@ -51,21 +52,43 @@ public:
     int clipTextMulti( char *, std::list<char*>*, float, int);
     static void repositionText( std::vector<GLfloat>*, int, int );
 
-    FT_Library   library;
-    FT_Face      face;
-    FT_GlyphSlot slot;
-    FT_Bitmap*   bitmap;
-    GLuint       atlas_texture;
-    GLubyte*     atlas_data;
-    short int    max_height;
-    bool origin_topleft;
+
+private:
+
+    FT_Library    library;
+    FT_Face       face;
+    FT_GlyphSlot  slot;
+    FT_Bitmap*    bitmap;
+    GLuint        atlas_texture;
+    GLubyte*      atlas_data;
+    short int     max_height;
+    bool          origin_topleft;
     glyph_matrix  *my_matrix;
 
     std::vector<glyph_info> glyphs;
-
-    //need to figure out lists
+    //need to figure out lists //?? wtf
 
     inline int next_p2 (int a ) { int rval=1; while(rval<a) rval<<=1; return rval; }
+
+};
+
+
+typedef struct font_list_pointerss {
+    std::list<std::vector<GLfloat> *> vec_vertex_list;
+    std::vector<GLfloat> *vec_texture_list;
+    std::vector<GLfloat> *vec_color_list;
+} font_list_pointers;
+
+class freetype_font_controller {
+public:
+
+    void render();
+
+
+private:
+
+    std::map<GLuint, font_pointers>
+
 
 };
 
