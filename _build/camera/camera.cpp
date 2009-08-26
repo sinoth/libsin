c0f2c89d1784362eda86a258526a2d453dc9cc35
svn <svn@d9b11391-c972-1f02-32a5-2912da4355a6>
2009-08-26 14:32:52 +0000

    added camera structure

---
+++ _build/camera/camera.cpp
@@ -0,0 +1,346 @@
+#include <GL/glfw.h>
+#include <math.h>
+
+#include "camera.h"
+
+//////////////////////////////////////////
+// Quaternion camera code from Vic Hollis
+// http://nehe.gamedev.net/data/lessons/lesson.asp?lesson=Quaternion_Camera_Class
+//////////////////////////////////////////////////////////////////////////////////
+
+camera::camera() {
+    f_heading_degrees = 0;
+    f_pitch_degrees = -30;
+    f_max_heading_rate = 5.0f;
+    f_max_pitch_rate = 5.0f;
+    f_max_pitch = 85.0f;
+    f_max_velocity = 5.0f;
+    f_max_strafe_velocity = 5.0f;
+    f_velocity = 0.0f;
+    f_strafe_velocity = 0.0f;
+    p_position.set(0,50,100);
+
+    for ( int i = 0; i < 15; i++ ) {
+        af_Matrix_rot[i] = 0.0f;
+        af_Matrix_pos[i] = 0.0f;
+    }
+    af_Matrix_rot[15] = 1.0f;
+    af_Matrix_pos[15] = 1.0f;
+
+    recalculateAngle();
+
+}
+
+
+void camera::setPerspective()
+{
+
+    // Let OpenGL set our new prespective on the world!
+    glMultMatrixf(af_Matrix_rot);
+
+    // Translate to our new position.
+    glTranslatef(-p_position.x, -p_position.y, -p_position.z);
+
+}
+
+
+void camera::updatePosition() {
+
+	static vector v_temp_dir, v_temp_strafe;
+
+    // Scale the direction by our speed.
+	v_temp_dir = v_direction * f_velocity;
+	v_temp_strafe = v_strafe_direction * f_strafe_velocity;
+
+	// Increment our position by the vector
+	p_position.x += v_temp_dir.x;
+	p_position.y += v_temp_dir.y;
+	p_position.z += v_temp_dir.z;
+    p_position.x += v_temp_strafe.x;
+	p_position.y += v_temp_strafe.y;
+	p_position.z += v_temp_strafe.z;
+
+
+    //af_Matrix_rot[12] = -p_position.x;
+    //af_Matrix_rot[13] = -p_position.z;
+    //af_Matrix_rot[14] = -p_position.y;
+    //af_Matrix_rot[15] = 1;
+
+
+}
+
+
+void camera::changePitch(GLfloat degrees)
+{
+
+	if(fabs(degrees) < f_max_pitch_rate)
+	{
+		// Our pitch is less than the max pitch rate that we
+		// defined so lets increment it.
+		f_pitch_degrees += degrees;
+	}
+	else
+	{
+		// Our pitch is greater than the max pitch rate that
+		// we defined so we can only increment our pitch by the
+		// maximum allowed value.
+		if(degrees < 0)
+		{
+			// We are pitching down so decrement
+			f_pitch_degrees -= f_max_pitch_rate;
+		}
+		else
+		{
+			// We are pitching up so increment
+			f_pitch_degrees += f_max_pitch_rate;
+		}
+	}
+
+	// We don't want our pitch to run away from us. Although it
+	// really doesn't matter I prefer to have my pitch degrees
+	// within the range of -360.0f to 360.0f
+	if(f_pitch_degrees > 360.0f)
+	{
+		f_pitch_degrees -= 360.0f;
+	}
+	else if(f_pitch_degrees < -360.0f)
+	{
+		f_pitch_degrees += 360.0f;
+	}
+
+	if ( f_pitch_degrees > f_max_pitch ) { f_pitch_degrees = f_max_pitch; }
+	else if ( f_pitch_degrees < -f_max_pitch ) { f_pitch_degrees = -f_max_pitch; }
+
+}
+
+void camera::changeHeading(GLfloat degrees)
+{
+	if(fabs(degrees) < f_max_heading_rate)
+	{
+        f_heading_degrees += degrees;
+	}
+	else
+	{
+        if ( degrees < 0 )
+            f_heading_degrees -= f_max_pitch_rate;
+        else
+            f_heading_degrees += f_max_pitch_rate;
+	}
+
+	// We don't want our heading to run away from us either. Although it
+	// really doesn't matter I prefer to have my heading degrees
+	// within the range of -360.0f to 360.0f
+	if(f_heading_degrees > 360.0f)
+	{
+		f_heading_degrees -= 360.0f;
+	}
+	else if(f_heading_degrees < 0.0f)
+	{
+		f_heading_degrees += 360.0f;
+	}
+}
+
+void camera::recalculateAngle() {
+
+	// Make the Quaternions that will represent our rotations
+	q_pitch.createFromAxisAngle(1.0f, 0.0f, 0.0f, f_pitch_degrees);
+	q_heading.createFromAxisAngle(0.0f, 1.0f, 0.0f, f_heading_degrees);
+
+	(q_heading * q_pitch).createMatrix(af_Matrix_rot);
+	(q_pitch * q_heading).createMatrix(af_Matrix_pos);
+
+	v_direction.x = af_Matrix_pos[8];
+	v_direction.y = af_Matrix_pos[9];
+    v_direction.z = -af_Matrix_pos[10];
+	v_strafe_direction.x = af_Matrix_pos[0];
+	v_strafe_direction.y = af_Matrix_pos[1];
+    v_strafe_direction.z = -af_Matrix_pos[2];
+	v_up.x = af_Matrix_pos[4];
+	v_up.y = af_Matrix_pos[5];
+    v_up.z = -af_Matrix_pos[6];
+
+}
+
+void camera::changeVelocity(GLfloat vel)
+{
+	if(fabs(vel) < f_max_velocity)
+	{
+		// Our velocity is less than the max velocity increment that we
+		// defined so lets increment it.
+		f_velocity += vel;
+	}
+	else
+	{
+		// Our velocity is greater than the max velocity increment that
+		// we defined so we can only increment our velocity by the
+		// maximum allowed value.
+		if(vel < 0)
+		{
+			// We are slowing down so decrement
+			f_velocity -= -f_max_velocity;
+		}
+		else
+		{
+			// We are speeding up so increment
+			f_velocity += f_max_velocity;
+		}
+	}
+}
+
+void camera::changeStrafeVelocity(GLfloat vel)
+{
+	if(fabs(vel) < f_max_strafe_velocity)
+	{
+		// Our velocity is less than the max velocity increment that we
+		// defined so lets increment it.
+		f_strafe_velocity += vel;
+	}
+	else
+	{
+		// Our velocity is greater than the max velocity increment that
+		// we defined so we can only increment our velocity by the
+		// maximum allowed value.
+		if(vel < 0)
+		{
+			// We are slowing down so decrement
+			f_strafe_velocity -= -f_max_strafe_velocity;
+		}
+		else
+		{
+			// We are speeding up so increment
+			f_strafe_velocity += f_max_strafe_velocity;
+		}
+	}
+}
+
+void camera::setVelocity(GLfloat vel) { f_velocity = vel; }
+void camera::setStrafeVelocity(GLfloat vel) { f_strafe_velocity = vel; }
+
+
+/*
+void camera::updatePositionXPlane(float mouse_inx, float mouse_iny) {
+
+	// Create a matrix from the pitch Quaternion and get the j vector
+	// for our direction.
+	//q_pitch.createMatrix(af_Matrix);
+	//v_direction.y = af_Matrix[9];
+
+	// Combine the heading and pitch rotations and make a matrix to get
+	// the i and j vectors for our direction. ???
+	q_temp = q_heading;
+	q_temp.createMatrix(af_Matrix_pos);
+	v_direction.x = af_Matrix_pos[8];
+	v_direction.y = af_Matrix_pos[9];
+    v_direction.z = af_Matrix_pos[10];
+	v_strafe_direction.x = af_Matrix_pos[0];
+	v_strafe_direction.y = af_Matrix_pos[1];
+    v_strafe_direction.z = af_Matrix_pos[2];
+
+    //printf("%f,%f,%f\n",v_direction.x,v_direction.y,v_direction.z);
+
+    // Scale the direction by our speed.
+	v_direction *= -mouse_iny;
+	v_strafe_direction *= mouse_inx;
+
+	// Increment our position by the vector
+	p_position.x += v_direction.x;
+	//p_position.y += v_direction.y;
+	p_position.z += v_direction.z;
+    p_position.x += v_strafe_direction.x;
+	//p_position.y += v_strafe_direction.y;
+	p_position.z += v_strafe_direction.z;
+
+}
+
+
+
+void camera::updatePositionYPlane(float mouse_inx, float mouse_iny) {
+
+	// Create a matrix from the pitch Quaternion and get the j vector
+	// for our direction.
+	//q_pitch.createMatrix(af_Matrix);
+	//v_direction.y = af_Matrix[9];
+
+	// Combine the heading and pitch rotations and make a matrix to get
+	// the i and j vectors for our direction. ???
+	q_temp = q_up;
+	q_temp.createMatrix(af_Matrix_pos);
+	v_direction.x = af_Matrix_pos[8];
+	v_direction.y = af_Matrix_pos[9];
+    v_direction.z = af_Matrix_pos[10];
+	v_strafe_direction.x = af_Matrix_pos[0];
+	v_strafe_direction.y = af_Matrix_pos[1];
+    v_strafe_direction.z = af_Matrix_pos[2];
+
+    //printf("%f,%f,%f\n",v_direction.x,v_direction.y,v_direction.z);
+
+    // Scale the direction by our speed.
+	v_direction *= -mouse_iny;
+	v_strafe_direction *= mouse_inx;
+
+	// Increment our position by the vector
+	//p_position.x += v_direction.x;
+	p_position.y += v_direction.y;
+	//p_position.z += v_direction.z;
+    //p_position.x += v_strafe_direction.x;
+	p_position.y += v_strafe_direction.y;
+	//p_position.z += v_strafe_direction.z;
+
+}
+*/
+
+void camera::setInternals(float inangle, float inratio, float innear, float infar) {
+
+	// store the information
+	f_ratio = inratio;
+	f_nearP = innear;
+	f_farP  = infar;
+	f_fov_angle = inangle * ANG2RAD;
+
+	// compute width and height of te near section
+	f_tang = (float)tan(f_fov_angle) ;
+	f_height = innear * f_tang;
+	f_width = f_height * inratio;
+
+	f_sphereFactorY = 1.0/cos(f_fov_angle);
+	f_sphereFactorX = 1.0/cos(atan(f_tang*inratio));
+}
+
+
+
+int camera::pointInView( point &p, float radius ) {
+
+	static float d1,d2;
+	static float az,ax,ay,zz1,zz2;
+	static int result;
+    static vector v;
+
+	result = 2;
+	v = p.subToVector(p_position);
+
+	az = v.dotproduct(v_direction);
+	if (az > f_farP + radius || az < f_nearP-radius)
+		return(0);
+
+	ax = v.dotproduct(v_strafe_direction);
+	zz1 = az * f_tang * f_ratio;
+	d1 = f_sphereFactorX * radius;
+	if (ax > zz1+d1 || ax < -zz1-d1)
+		return(0);
+
+	ay = v.dotproduct(v_up);
+	zz2 = az * f_tang;
+	d2 = f_sphereFactorY * radius;
+	if (ay > zz2+d2 || ay < -zz2-d2)
+		return(0);
+
+	if (az > f_farP - radius || az < f_nearP+radius)
+		result = 1;
+	if (ay > zz2-d2 || ay < -zz2+d2)
+		result = 1;
+	if (ax > zz1-d1 || ax < -zz1+d1)
+		result = 1;
+
+	return(result);
+
+}
---
+++ include/sincamera.h
@@ -0,0 +1,67 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "prim.h"
+
+#define ANG2RAD 3.14159265358979323846/360.0
+
+////////////////////////////////////////////////////////////////////////////////
+// frustum code courtesy of:
+// http://www.lighthouse3d.com/opengl/viewfrustum/index.php?rimp
+////////////////////////////////////////////////////////////////////////////////
+
+class camera {
+    public:
+    camera();
+    void setPerspective();
+    void changePitch(GLfloat);
+    void changeHeading(GLfloat);
+    void changeVelocity(GLfloat);
+    void changeStrafeVelocity(GLfloat);
+    void setVelocity(GLfloat);
+    void setStrafeVelocity(GLfloat);
+    void updatePosition();
+    void setPosition( btVector3 in ) { p_position.x = in.getX(); p_position.y = in.getY(); p_position.z = in.getZ(); }
+    //void updatePositionXPlane(float,float);
+    //void updatePositionYPlane(float,float);
+    void recalculateAngle();
+	int pointInView(point &p, float );
+    void setInternals(float, float, float, float);
+
+
+    GLfloat f_pitch_degrees;
+    GLfloat f_max_pitch_rate;
+    GLfloat f_max_pitch;
+    GLfloat f_heading_degrees;
+    GLfloat f_max_heading_rate;
+    GLfloat f_velocity;
+    GLfloat f_max_velocity;
+    GLfloat f_strafe_velocity;
+    GLfloat f_max_strafe_velocity;
+
+    GLfloat af_Matrix_rot[16];
+    GLfloat af_Matrix_pos[16];
+
+    quaternion q_pitch;
+    quaternion q_heading;
+    point p_position;
+    vector v_direction;
+    vector v_strafe_direction;
+    vector v_up;
+
+    //frustum funstuff
+    GLfloat f_fov_angle;
+    GLfloat f_nearP;
+    GLfloat f_farP;
+    GLfloat f_ratio;
+    GLfloat f_width;
+    GLfloat f_height;
+    GLfloat f_tang;
+    GLfloat f_sphereFactorX;
+    GLfloat f_sphereFactorY;
+
+};
+
+
+
+#endif //\/ CAMERA_H
