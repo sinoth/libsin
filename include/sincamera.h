adc6f759709440e548c5a8533fb587499b4b4af1
svn <svn@d9b11391-c972-1f02-32a5-2912da4355a6>
2009-12-11 00:11:30 +0000

    arcball camera works!

--- include/sincamera.h
+++ include/sincamera.h
@@ -41,88 +41,32 @@ public:
     void arcSetCenter(const vec3f &in) { arc_center = in; }
     void arcSetFacing(const vec3f &in) { arc_facing = in; }
     void arcSetRotation(const quaternion &in) { q_arc_rotation = in; }
-    void arcSetUp(const vec3f &in) { arc_up = in; }
 
     void arcSpinMouseX(const float &in) {
                 static quaternion temp;
                 static vec3f up(0,1,0);
-
-                //temp.createFromAxisAngle( arc_up, in );
                 temp.createFromAxisAngle( up, in );
-
-                q_arc_rotation = temp * q_arc_rotation;
-                //q_arc_rotation *= temp;
-
+                q_arc_rotation *= temp;
                 q_arc_rotation.normalize();
-
                 arcRecalculate(); }
 
     void arcSpinMouseY(const float &in) {
                 static quaternion temp;
                 static vec3f right(-1,0,0);
-
-                //temp.createFromAxisAngle( arc_strafe, in );
                 temp.createFromAxisAngle( right, in );
-
-                q_arc_rotation = temp * q_arc_rotation;
-                //q_arc_rotation *= temp;
-
+                q_arc_rotation *= temp;
                 q_arc_rotation.normalize();
-
                 arcRecalculate(); }
 
     void arcRecalculate() {
                 q_arc_rotation.toMatrix(af_Matrix_rot);
-                q_arc_rotation.conjugate().toMatrix(af_Matrix_pos);
 
+                arc_facing.x = -af_Matrix_rot[2];
+                arc_facing.y = -af_Matrix_rot[6];
+                arc_facing.z = -af_Matrix_rot[10];
 
-                arc_facing.x = af_Matrix_rot[8];
-                arc_facing.y = af_Matrix_rot[9];
-                arc_facing.z = af_Matrix_rot[10];
-                arc_strafe.x = af_Matrix_rot[0];
-                arc_strafe.y = af_Matrix_rot[1];
-                arc_strafe.z = af_Matrix_rot[2];
-                arc_up.x = af_Matrix_rot[4];
-                arc_up.y = af_Matrix_rot[5];
-                arc_up.z = af_Matrix_rot[6];
-
-
-                /*
-                arc_facing.x = af_Matrix_rot[2];
-                arc_facing.y = af_Matrix_rot[6];
-                arc_facing.z = af_Matrix_rot[10];
-                arc_strafe.x = af_Matrix_rot[0];
-                arc_strafe.y = af_Matrix_rot[4];
-                arc_strafe.z = af_Matrix_rot[8];
-                arc_up.x = af_Matrix_rot[1];
-                arc_up.y = af_Matrix_rot[5];
-                arc_up.z = af_Matrix_rot[9];
-                */
-
-
-/*
-0 1 2 3
-4 5 6 7
-8 9 1011
-12131415
-
-0 4 8 12
-1 5 9 13
-2 6 1014
-3 7 1115
-*/
-                p_position = (arc_facing * arc_radius + arc_center);
-/*
-                // Make the Quaternions that will represent our rotations
-                q_pitch.createFromAxisAngle(1.0f, 0.0f, 0.0f, f_pitch_degrees);
-                q_heading.createFromAxisAngle(0.0f, 1.0f, 0.0f, f_heading_degrees);
-
-                (q_heading * q_pitch).toMatrix(af_Matrix_rot);
-                (q_pitch * q_heading).toMatrix(af_Matrix_pos);
-*/
-                printf("*** distance: %f\n", (p_position - arc_center).length());
+                p_position = (-arc_facing * arc_radius + arc_center);
             }
-
 
 
 //private:
@@ -150,8 +94,6 @@ public:
     //arcball-esque vars
     vec3f arc_center;
     vec3f arc_facing;
-    vec3f arc_up;
-    vec3f arc_strafe;
     float arc_radius;
     quaternion q_arc_rotation;
 
